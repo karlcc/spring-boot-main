605914f8b306d0b7b964ff47432c8259110050be
Karl Chow <karl.logon@gmail.com>
2024-02-18 18:25:25 +0800

    up to testing env

---
+++ next-equities-server/src/main/java/com/karl/equities/EquitiesApplication.java
@@ -0,0 +1,71 @@
+package com.karl.equities;
+
+import com.karl.equities.auth.AuthenticationService;
+import com.karl.equities.auth.RegisterRequest;
+import com.karl.equities.book.BookRequest;
+import com.karl.equities.book.BookService;
+import com.karl.equities.watchlist.WatchlistRequest;
+import com.karl.equities.watchlist.WatchlistService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import static com.karl.equities.user.Role.ADMIN;
+import static com.karl.equities.user.Role.MANAGER;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+public class EquitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquitiesApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			AuthenticationService service,
+			WatchlistService watchlistService
+//			BookService bookService
+	) {
+		return args -> {
+			var admin = RegisterRequest.builder()
+					.username("Admin")
+					.email("admin@mail.com")
+					.password("password")
+					.role(ADMIN)
+					.build();
+			System.out.println("Admin token: " + service.register(admin).getAccessToken());
+
+			var manager = RegisterRequest.builder()
+					.username("Admin")
+					.email("manager@mail.com")
+					.password("password")
+					.role(MANAGER)
+					.build();
+			System.out.println("Manager token: " + service.register(manager).getAccessToken());
+
+			var watchlist1 = WatchlistRequest.builder()
+					.issue("SPY")
+					.build();
+			watchlistService.save(watchlist1);
+			System.out.println("watchlist 1: "+ watchlist1.getIssue());
+
+//			var book1 = BookRequest.builder()
+//					.author("John Doe")
+//					.isbn("1234567890")
+//					.build();
+//			bookService.save(book1);
+//			System.out.println("Book 1: "+ book1.getAuthor());
+
+//			var book2 = BookRequest.builder()
+//					.author("Jane Smith")
+//					.isbn("0987654321")
+//					.build();
+//			bookService.save(book2);
+//			System.out.println("Book 2: "+ book2.getAuthor());
+
+		};
+	}
+}
